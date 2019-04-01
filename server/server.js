991609d381ad79e39eb77a73818a542bf55d03fd
Hiep Nguyen <thienhiep1711@users.noreply.github.com>
2019-04-01 15:08:08 +0700

    fix error

--- server/server.js
+++ server/server.js
@@ -5,13 +5,11 @@ const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-var server = http.createServer(app);
-
 
 app.use(express.static(publicPath));
 
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is up on ${port}`);
 })
 
