4cfdb5e866cdea10fb4186dc2883b10ae0c0a01c
Hiep Nguyen <thienhiep171193@gmail.com>
2020-05-15 23:01:18 +0700

    Update viotrans page

---
+++ public/viotrans/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _dom = require("lib/dom");
+
+var _utils = require("lib/utils");
+
+var _initModules = _interopRequireDefault(require("lib/init-modules"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _dom.ready)((0, _utils.pipe)(_initModules["default"]));
+//# sourceMappingURL=../maps/script/main.js.map
